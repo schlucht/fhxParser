34dbb072f05c753454de564d521ef9d60935edff
Schmid Lothar <web@jagolo.ch>
2023-01-19 05:12:34 +0100

    Install NUXT frontend

--- frontend/app.vue
+++ frontend/app.vue
@@ -1,5 +1,40 @@
 <template>
   <div>
-    <NuxtWelcome />
+    <header>
+      <h1>Delta-V Parameters</h1>
+      <nav>
+        <ul>
+          <li><a href="/home">Startseite</a></li>
+          <li><a href="/recipe">Produkt Rezepte</a></li>
+          <li><a href="/unit">Standard Units</a></li>
+          <li><a href="/about">Die Seite</a></li>
+        </ul>
+      </nav>
+    </header>
+    <main>
+      <section id="unitslist">
+        <h2>Alle Standard Units</h2>
+        <p>
+        <article>
+          <h2>Q2000</h2>
+          <p>
+
+          </p>
+          <footer></footer>
+        </article>
+        </p>
+      </section>
+      <section id="recipelist">
+        <h2>Produkte mit Rezepte</h2>
+      </section>
+    </main>
+    <footer>
+      <p>Copyright &copy;</p>
+      <p>Schmid Lothar</p>
+      <p>1.2023</p>
+    </footer>
   </div>
 </template>
+<style lang="scss" scoped>
+
+</style>
