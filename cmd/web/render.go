28094553c64de84db0a2bf79d4bec0539055092b
schlucht <web@jagolo.ch>
2023-07-07 14:14:22 +0000

    refact render slice

--- cmd/web/render.go
+++ cmd/web/render.go
@@ -74,8 +74,7 @@ func (app *application) parseTemplate(partials []string, page, templateToRender 
 			partials[i] = fmt.Sprintf("templates/partials/%s.partial.tmpl", x)
 		}
 	}
-	partials = append(partials, "templates/base.layout.tmpl")
-	partials = append(partials, templateToRender)
+	partials = append(partials, "templates/base.layout.tmpl", templateToRender)
 	if len(partials) > 0 {
 		t, err = template.New(fmt.Sprintf("%s.page.tmpl", page)).Funcs(functions).ParseFS(templateFS, partials...)
 
