a4e3f7a67217576a26d6ecbfe622741d835ce1d4
schlucht <web@jagolo.ch>
2023-07-11 13:04:54 +0000

    add ots message template

--- static/js/alert.component.js
+++ static/js/alert.component.js
@@ -1,61 +1,95 @@
 const template = document.createElement('template');
 
-template.innerHTML = `
+template.innerHTML = /*html*/`
     <style>
-        .alert {
+        :host {
+            display: none;
             position: relative;
             text-align: center;
             padding: 1rem;
             width: 100%;
             margin: 1rem;      
         }
-        .alert.critical {
+        :host(.critical) {
             background-color: rgba(255,0,0,0.2);
             border: solid 1px red;
             border-radius: 3px;
             color: red;
         }
-        .alert.success {
+        :host(.success) {
             background-color: rgba(0,255,0,0.2);
             border: solid 1px green;
             border-radius: 3px;
             color: green;
         }
-        .alert span {
+        :host span {
             position: absolute;
             font-size: 2rem;
             right: 10px;
             top: 3px;
             cursor: pointer;
         }
-        .alert span:hover {
+        :host span:hover {
             font-size: 2.2rem;
         }
 
     </style>
 
-    <div id="alert" class="alert">Hier kommt die Meldung!
+    <p>
+        <slot></slot>
         <span>x</span>
-    </div>
+    </p>
 `
 
 class OtsAlertMessage extends HTMLElement {
-
-    shadow = null;
+    static get observedAttribute() { return ['hidden']; }
 
     constructor() {
         super();
-
-        this.shadow = this.attachShadow({ mode: 'open' })
+        this._shadow = this.attachShadow({ mode: 'open' });
+        this._tmp = document.querySelector('ots-alert-message');
+        this._internals = this.attachInternals();
     }
 
-    connectedCallback() {       
-        const alert = this.shadow.getElementById('alert');
-        alert.classList.add(this.getAttribute('type'));
-        this.shadow.appendChild(template.content.cloneNode(true));
-        this.shadow.querySelector('span').addEventListener('click', function() {
-            alert.style.display = 'none';
-        })
+    connectedCallback() {
+        this._shadow.appendChild(template.content.cloneNode(true))
+
+        const type = this._tmp.getAttribute('type')
+
+        if (type === 'success') {
+            this._tmp.classList.add('success')
+        } else if (type === 'critical') {
+            this._tmp.classList.add('critical')
+        } else {
+            this._tmp.classList.add('success')
+        }
+
+        const close = this.shadowRoot.querySelector('span')
+
+        close.addEventListener('click', (e) => {
+            this._tmp.style.display = 'none';
+            this._tmp.removeAttribute('hidden');
+        });
+    }
+
+
+    get hidden() { console.log(this.hasAttribute('hidden')); return this.hasAttribute('hidden') }
+    set hidden(flag) {
+        if (flag) {
+            this.setAttribute('hidden', '');
+        } else {
+            this.removeAttribute('hidden');
+        }
+    }
+
+    attributeChangedCallback(name, oldValue, newValue) {
+        console.log(name);
+        if (name === 'hidden') {
+            this._tmp.style.display = 'none';
+            return;
+        }
+        this._tmp.style.display = 'block';
+
     }
 }
 
